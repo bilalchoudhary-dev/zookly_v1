376bd6b00becb44d33ee395513e7e7eb01567d79
BIlalChaudhary <mobilal560@gmail.com>
2026-02-03 18:45:20 +0530

    added some more icons

--- src/lib/icons.js
+++ src/lib/icons.js
@@ -8,6 +8,11 @@ export const getIconByUrl = (url) => {
   if (lowUrl.includes('twitter.com') || lowUrl.includes('x.com')) return 'Twitter';
   if (lowUrl.includes('linkedin.com')) return 'Linkedin';
   if (lowUrl.includes('facebook.com')) return 'Facebook';
+  if (lowUrl.includes('tiktok.com')) return 'Tiktok';
+  if (lowUrl.includes('whatsapp.com')) return 'message-square'; 
+  if (lowUrl.includes('telegram.me') || lowUrl.includes('t.me')) return 'Send';
+  if (lowUrl.includes('discord.com')) return 'rss';
+  if (lowUrl.includes('reddit.com')) return 'bot-message-square';
   
   return 'Globe'; // Default icon
 };
